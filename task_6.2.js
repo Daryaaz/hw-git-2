0fe2620bcc8c84070acc78c27ead05db4ceb7c8a
Daryaaz <darja.zibkina@gmail.com>
2023-01-26 13:53:44 +0200

    added homework 6.2

---
+++ task_6.2.js
@@ -0,0 +1,24 @@
+function increaseBalance(x, y) {
+    return x += y;
+  }
+  console.log(increaseBalance(3000, 700));
+  
+
+  function decreaseBalance(x, y) {
+  return x -= y;
+  }
+  console.log(decreaseBalance(3000, 700));
+
+
+  
+  function divideBalanceByAccounts(x, y, c) {
+    return (x += y) /c
+  }
+  console.log(divideBalanceByAccounts(3000, 4000, 2));
+  
+  
+  
+  function getRestAfterDivision(x, y) {
+    return x % y;
+  }
+  console.log(getRestAfterDivision(7000, 3))
