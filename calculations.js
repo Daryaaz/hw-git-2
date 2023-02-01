9e3abf6a780d9748c5107586638e853fff199982
Daryaaz <darja.zibkina@gmail.com>
2023-02-01 18:20:52 +0200

    updated homework

--- calculations.js
+++ calculations.js
@@ -1,30 +1,28 @@
 function increaseBalance(x, y) {
-  // x = x + y;
-  return x + y;
+  x = x + y;
+  return x;
 }
-console.log(increaseBalance(3000, 700));
+
 
 function decreaseBalance(x, y) {
-// x = x - y;
-return x - y;
+x = x - y;
+return x;
 }
-console.log(decreaseBalance(3000, 700));
 
-function divideBalanceByAccounts(x, y, c) {
-  // x = x / y;
-  return (x + y)/c;
-  // console.log("x");
+
+function divideBalanceByAccounts(x, y) {
+  x = x / y;
+  return x;
 }
-console.log(divideBalanceByAccounts(3000, 4000, 2));
+console.log("x");
 
 
 function getRestAfterDivision(x, y) {
-  // x = x % y;
-  return x % y;
+  x = x % y;
+  return x;
 }
-console.log(getRestAfterDivision(7000, 3))
 
-// console.log(increaseBalance(3000, 700));
-// console.log(decreaseBalance(3000, 700));
-// console.log(divideBalanceByAccounts(3000, 2, 4000));
-// console.log(getRestAfterDivision(7000, 3));
+console.log(increaseBalance(3000, 700));
+console.log(decreaseBalance(3000, 700));
+console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(getRestAfterDivision(7000, 3));
