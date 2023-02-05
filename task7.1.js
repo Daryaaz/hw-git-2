0076b905ee8534223c8c93f2a58978169aff50b4
Daryaaz <darja.zibkina@gmail.com>
2023-02-05 10:13:45 +0200

    updated HW 7.1

--- task7.1.js
+++ task7.1.js
@@ -1,15 +1,27 @@
-let balance = 100;
-let parcelCost = 100;
-let receivedCode = "A001DFX0";
-let enteredCode = "A001DFX0";
-let parcelMachine = [null, null, null, '4-yourparcel', null];
-
-if (balance>=parcelCost){
-    if(receivedCode==enteredCode){
-        balance-=parcelCost;
-        console.log(`You have received parcel from cell Nr.4. 
-        Your balance is ${balance}`);
-    }else{
-        console.log("Invalid code")
-    }
+let receiver = {
+    balance: 100,
+    parcelCost: 100,
+    receivedCode: "A001DFX0",
 }
+const parcelMachine = [null, null, null, "A001DFX0", null];
+
+for(let i=0; i<parcelMachine.length; i++){
+    if(parcelMachine[i] === receiver.receivedCode) {
+        receiver.balance = receiver.balance - 100
+        console.log(`You have received parcel from cell Nr.4. 
+        Your balance is ${receiver.balance}`);
+    } else {
+        console.log("Invalid code");
+    }
+        
+}
+
+// if (balance>=parcelCost){
+//     if(receivedCode==enteredCode){
+//         balance-=parcelCost;
+//         console.log(`You have received parcel from cell Nr.4. 
+//         Your balance is ${balance}`);
+//     }else{
+//         console.log("Invalid code")
+//     }
+// }
